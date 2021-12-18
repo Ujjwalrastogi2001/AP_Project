02ef0fcc31da32789c08dedd1339306ff04dcaae
Ujjwal Rastogi <ujjwal20546@iiitd.ac.in>
2021-12-18 11:27:57 +0530

    restored old version  with start button

--- src/main/java/com/example/ap_project/HelloApplication.java
+++ src/main/java/com/example/ap_project/HelloApplication.java
@@ -3,15 +3,19 @@ package com.example.ap_project;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import com.example.ap_project.HelloController;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("board.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("front.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Snakes And Ladder");
         stage.setResizable(false);
